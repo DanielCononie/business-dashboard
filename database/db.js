66c649eae83d778eba823effcb8435d10790a08d
Daniel Cononie <danielcononie1278@gmail.com>
2023-11-11 16:04:43 -0500

    new changes

--- database/db.js
+++ database/db.js
@@ -1,19 +1,25 @@
-import { Pool } from "pg";
-import dotenv from 'dotenv'
-dotenv.config()
+// import { Pool } from "pg";
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-const portNum = process.env.PORTNUM
-const db_name = process.env.DB_NAME
-const password = process.env.DB_PASSWORD
-const host = process.env.HOST
+// const portNum = process.env.PORTNUM
+// const db_name = process.env.DB_NAME
+// const password = process.env.DB_PASSWORD
+// const host = process.env.HOST
 
-    const pool = new Pool({
-        user: "postgres",
-        password: password,
-        host: host,
-        port: portNum,
-        database: db_name
-    });
+//     const pool = new Pool({
+//         user: "postgres",
+//         password: password,
+//         host: host,
+//         port: portNum,
+//         database: db_name
+//     });
 
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  connectionString: "postgres://default:ES6KCG0oZqQt@ep-polished-shape-44430954-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+})
 
 module.exports = pool;
